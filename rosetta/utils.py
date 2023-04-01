de41fb7501586c6705c1e58e1f13230fcdc4d02f
William <william.ribeiro@jusbrasil.com.br>
2023-04-01 16:44:00 -0300

    fix typo

--- rosetta/utils.py
+++ rosetta/utils.py
@@ -10,6 +10,6 @@ def parse_response(response: dict) -> str:
     return text or message
 
 
-def mount_payload(current_leanguage: str, target_leaguage: str, text: str) -> str:
+def mount_payload(current_languages: str, target_leaguage: str, text: str) -> str:
     """This method return an stringified url with this format: source_language=en&target_language=pt&text=What%20is%20your%20name%3F"""
-    return f"source_language={current_leanguage}&target_language={target_leaguage}&text={quote(text)}"
+    return f"source_language={current_languages}&target_language={target_leaguage}&text={quote(text)}"
