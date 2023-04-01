d9646bfbb3192876358764537c2535bee21d6c8e
William <william.ribeiro@jusbrasil.com.br>
2023-04-01 16:44:46 -0300

    fix error when receive a text

--- rosetta/__main__.py
+++ rosetta/__main__.py
@@ -4,10 +4,12 @@ from rosetta import api, cli, utils
 def main():
     args = cli.cli()
     payload = utils.mount_payload(args.i, args.o, args.t)
-    response = api.post_translate(payload)
-    text = utils.parse_response(response)
+    response: dict | str = api.post_translate(payload)
+    if isinstance(response, dict):
+        text = utils.parse_response(response)
+    else:
+        text = response
     print(text)
-
 
 
 if __name__ == "__main__":
