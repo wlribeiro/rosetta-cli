7f833561c0fb2926afcf1da218414ab24669a52c
William <william.ribeiro@jusbrasil.com.br>
2023-04-01 18:58:37 -0300

    lint

--- test/test_cli.py
+++ test/test_cli.py
@@ -4,11 +4,13 @@ from unittest.mock import patch
 from rosetta import cli
 
 
-@patch('argparse.ArgumentParser.parse_args',
-       return_value=argparse.Namespace(i='en', t='Hello world!', o='pt'))
+@patch(
+    "argparse.ArgumentParser.parse_args",
+    return_value=argparse.Namespace(i="en", t="Hello world!", o="pt"),
+)
 def test_cli(mock_args):
     result = cli.cli()
 
-    assert result.i == 'en'
-    assert result.t == 'Hello world!'
-    assert result.o == 'pt'
+    assert result.i == "en"
+    assert result.t == "Hello world!"
+    assert result.o == "pt"
